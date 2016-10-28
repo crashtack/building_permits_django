8ddcd6684ad0d1aac5cc1274f788d4dc1aea9ebd
crashtack <crashtack@gmail.com>
2016-10-28 13:06:44 -0700

    Created class based view for MapView, template, and url

--- maps/urls.py
+++ maps/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-
+from map.views import MapView
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(
+        r'^map$',
+        MapView.as_view(),
+        name='map'
+    ),
 
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,5 +1,30 @@
+import os
 from django.http import HttpResponse
+from django.views.generic import DetailView
+from django.contrib.gis.geoip2 import GeoIP2
 
 
 def index(request):
     return HttpResponse("Hello, world")
+
+
+class MapView(DetailView):
+    """
+    Shows a Map and with plotted points.
+    """
+
+    template = 'map.html'
+
+    def get_point(self, url):
+        point = GeoIP2()
+        return point.lat_lon(url)
+
+    def get_context_data(self, **kwargs):
+        context = super(MapView, self).get_context_data(**kwargs)
+        context['googleapikey'] = os.environ.get('GOOGLE_MAPS_API_KEY')
+        point = MapView.get_point(
+            'ec2-35-160-27-154.us-west-2.compute.amazonaws.com'
+        )
+        context['data'] = [{'lat': point[0], 'lng': point[1]}, ]
+
+        return context
